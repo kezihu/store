dac270a3fb31a86783d78e87ea760eb89cc94cef
kezihu <87291346+kezihu@users.noreply.github.com>
2021-08-09 09:26:36 +0800

    Delete day15 directory

--- day15/图片读取.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/8/6 15:47
-# @Author  : Kevin_liu
-# @Email   : 87281094@qq.com
-# @File    : 图片读取.py
-path = input('请输入图片的路径：\n')
-picture = (open(file=path, mode="rb")).read()
-photo = (open(file="picture.jpg", mode="wb")).write(picture)
